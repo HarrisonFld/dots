94ef5573105c0aa46e440389e965c51cf30d8667
HarrisonFld <fieldharrison@proton.me>
2024-09-28 20:56:54 -0400

    Initialized install script with stow_list for defining directories to stow

---
+++ script/install/install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+error_exit() {
+	printf "An error occured,\n$1,\nplease resolve the error given the instructions, exiting...\n"
+	exit
+}
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+DOTS_MANAGER="stow -t $HOME --no-folding --verbose=1"
+CFGDIR="$SCRIPT_DIR/../../config"
+
+# Check for directories in config/
+CONFIG_STOW=$(grep -v '^#' $CFGDIR/stow_list)
+CONFIG_DIRS=$(basename -a $(ls -d ${CFGDIR}/*/))
+ALL_UNIQUE=$(printf "$CONFIG_DIRS\n$CONFIG_STOW" | sort | uniq -u)
+DIRS_UNIQUE=$(printf "$ALL_UNIQUE\n$CONFIG_DIRS" | sort | uniq -d)
+STOW_UNIQUE=$(printf "$ALL_UNIQUE\n$CONFIG_STOW" | sort | uniq -d)
+
+# Check for directories in stow list that do not exist (if they don't, exit the script)
+if [[ "$STOW_UNIQUE" != "" ]]; then
+	error_exit "the directories \"$STOW_UNIQUE\" are defined in \"stow_list\" but do not exist"
+fi
+
+# Check for directories undefined in the script (if they exist, prompt to include them)
+if [[ $DIRS_UNIQUE != "" ]]; then
+	echo "The directories \"$DIRS_UNIQUE\" are in the config folder but not defined in \"stow_list\""
+	printf "Use the undefined directories anyway? [y/N]: "
+	read INCLUDE_DIRS_UNIQUE
+fi
+
+# Create the final list of directories
+FINAL_DIRS_LIST="$CONFIG_STOW"
+if [[ "${INCLUDE_DIRS_UNIQUE,,}" = "y" ]]; then
+	FINAL_DIRS_LIST="${CONFIG_STOW} ${DIRS_UNIQUE}"
+fi
+
+# TODO: Package requirements!
+
+for conf in $FINAL_DIRS_LIST
+do
+	$DOTS_MANAGER -d ${CFGDIR} ${conf}
+done
