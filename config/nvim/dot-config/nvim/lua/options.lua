40ce001caed355e2f611ad27d159ef3227dcb2d7
HarrisonFld <fieldharrison@proton.me>
2025-01-29 15:54:33 -0500

    Updated neovim config to remove line wrapping

--- config/nvim/dot-config/nvim/lua/options.lua
+++ config/nvim/dot-config/nvim/lua/options.lua
@@ -17,6 +17,7 @@ opt.showmatch = true --highlight pairs (ie. {},(),[]) when cursor is over them
 opt.termguicolors = true
 opt.cc = "110" --color column
 opt.ttyfast = true -- smoother scrolling
+opt.wrap = false
 
 --mouse
 opt.mouse = "v" --enable mouse usage
@@ -29,7 +30,7 @@ opt.shiftwidth = 4 --spaces sued for autoindent
 opt.expandtab = true --tabs convert to spaces
 opt.autoindent = true --main indentation across lines
 opt.paste = false --disable auto-indentation
-    
+
 opt.wildmode = "longest,list"
 opt.clipboard = "unnamedplus" --set clipboard to system clipboard
 
